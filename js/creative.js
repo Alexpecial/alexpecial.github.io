6c66d90d692df294e7e8f9e2fde368ae90e0e3d7
Alexpecial <larcwind@gmail.com>
2025-08-26 13:47:18 +0900

    Updating photo.html

--- js/creative.js
+++ js/creative.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Creative Bootstrap Theme[](http://startbootstrap.com)
+ * Start Bootstrap - Creative Bootstrap Theme
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
@@ -45,26 +45,7 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
-    // New code for photo.html: Initialize Masonry and handle photo clicks
-    if ($('.photo-grid').length) {
-        // Wait for images to load before initializing Masonry
-        $('.photo-grid').imagesLoaded(function() {
-            $('.photo-grid').masonry({
-                itemSelector: '.photo-item',
-                columnWidth: '.photo-item',
-                percentPosition: true,
-                gutter: 10
-            });
-        });
-
-        // Photo click event to open modal
-        $('.photo-item').on('click', function() {
-            var src = $(this).data('src');
-            var caption = $(this).data('caption');
-            $('#photoModal .modal-img').attr('src', src);
-            $('#photoModal .modal-caption').text(caption);
-            $('#photoModal').modal('show');
-        });
-    }
+    // --- Removed photo.html-specific Masonry & click handlers to avoid double init ---
+    // Photo page now handles its own grid initialization and modal logic.
 
 })(jQuery); // End of use strict
